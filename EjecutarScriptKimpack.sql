db1c1ede22d70311da1ebf8b629ce23b987f2f38
JocelynArgueta <81652961+JocelynArgueta@users.noreply.github.com>
2021-07-22 13:23:02 -0500

    Update EjecutarScriptKimpack.sql

--- EjecutarScriptKimpack.sql
+++ EjecutarScriptKimpack.sql
@@ -1,6 +1,10 @@
 SET NOCOUNT ON
 GO
 :On Error exit
-:r C:\Users\Servidor\Documents\Rene\Scripts\Kimpack\DML_ActualizaStatusOC_V2.sql
+:r C:\Users\AuxiliarSistemas\Documents\SQL Server Management Studio\Projects\Nueva carpeta\DDL_Roles.sql
+:r C:\Users\AuxiliarSistemas\Documents\SQL Server Management Studio\Projects\Nueva carpeta\DDL_Users.sql
+:r C:\Users\AuxiliarSistemas\Documents\SQL Server Management Studio\Projects\Nueva carpeta\DML_CFDI.sql
+:r C:\Users\AuxiliarSistemas\Documents\SQL Server Management Studio\Projects\Nueva carpeta\DML_InsertaRoles.sql
+:r C:\Users\AuxiliarSistemas\Documents\SQL Server Management Studio\Projects\Nueva carpeta\DML_InsertaUsuarios.sql
 PRINT 'Script Ejecutado correctamente'
 GO
